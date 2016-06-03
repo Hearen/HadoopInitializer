bc51084e954dc4d12bac01dbb52c4711482ed271
Hearen <LHearen@126.com>
2016-06-03 10:03:43 +0800

    updated for better portability

---
+++ tools/cgroup_controller.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#####################################################################################
+#Author      : LHearen
+#E-mail      : LHearen@gmail.com
+#Time        : Mon, 2016-05-30 09:26
+#Description : Used to configure cgroup by several configuration files;
+#####################################################################################
+
+. ../checker.sh
+
+IPS_FILE="../etc/ip_addresses"
+CG_FILE="../etc/cg*"
+#Root required
+function cgroup_configurer {
+    ips_file=$1
+    for ip in $(cat $ips_file)
+    do
+        echo "Start to copy cgroup configuration files to [$ip]"
+        scp $CG_FILE $ip:/etc/
+        echo "Start and enable cgconfig and cgred services in [$ip]"
+        ssh $ip "systemctl start cgconfig && systemctl enable cgconfig && systemctl start cgred && systemctl enable cgred"
+    done
+}
+
+echo "root privilege is required to run this script."
+check_permission 
+if [ $? -eq 0  ] 
+then 
+    echo "Permission Granted." 
+else 
+    echo "Leaving..."
+    exit 1
+fi
+cgroup_configurer $IPS_FILE
