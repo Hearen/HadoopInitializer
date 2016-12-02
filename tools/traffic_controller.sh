d1f6f337ab1659487ec4822f90ab6d065f3a4d4a
Hearen <LHearen@gmail.com>
2016-12-02 17:57:47 +0800

    refactor version 0.2

--- tools/traffic_controller.sh
+++ tools/traffic_controller.sh
@@ -1,122 +1,42 @@
 #!/bin/bash
 #-------------------------------------------
 # Author      : LHearen
-# E-mail      : LHearen@126.com
+# E-mail      : LHearen@gmail.com
 # Time        : 2016-06-14 11:29
-# Description : Used to throttle a sustained maximum rate of certain interface;
+# Description : Used to throttle a sustained maximum rate of an interface;
 #-------------------------------------------
 
-#Using a given test site to check the network and return
-#Http code 200 - okay
-function check_network {
-   test_site=$1
-   timeout_max=2    #seconds before time out
-   return_code=`curl -o /dev/null/ --connect-timeout $timeout_max -s -w %{http_code} $test_site`
-   echo $return_code
+# Throttle a specific interface to a specified speed by command `tc`
+function throttle_interface {
+    interface=$1
+    max_speed=$2
+    tc qdisc del dev $interface root
+    tc qdisc add dev $interface handle 1: root htb
+    tc class add dev $interface parent 1: classid 1:1 htb rate "$max_speed"mbps
+    highlighter_str 1 "Make some checking..."
+    tc qdisc show dev $interface && tc class show dev $interface
 }
 
-#Log in to access external network in ISCAS
-function login_network {
-    userName=${1:-"luosonglei14"}
-    password=${2:-"111111"}
-    while [ 1 ]
-    do
-        return_code=$(check_network "baidu.com")
-        if [ $return_code -eq 200 ]
-        then
-            tput setaf 2
-            echo "Network available"
-            tput sgr0
-            break
-        else
-            echo "Connection error"
-            echo "Trying to fix it..."
-            curl -d "username=$userName&password=$password&pwd=$password&secret=true" http://133.133.133.150/webAuth/ 1>/dev/null 2>/dev/null
-            sleep 1
-        fi
-    done
-}
-
-####################################
-#Check the network and try to fix it
-####################################
-function check_fix_network {
-    return_code=`check_network "baidu.com"`
-    if [ $return_code -eq 200 ]
-    then
-        echo "Network available"
-    else
-        echo "Connection error!"
-        echo "Trying to fix the network..."
-        service network restart > /dev/null  
-        login_network
-        return_code=`check_network "baidu.com"`
-        if [ $return_code -eq 200 ]
-        then
-            echo "Network available now."
-            return 0
-        else
-            echo "Failed to fix the connection."
-            echo "When you are using ISCAS Network, you might have to run login.sh first, enclosed with this program"
-            echo "To login in to access network."
-            echo "If you are using other networks, you should try to use browser to login or rewrite the login.sh to login."
-            echo "Remember sometimes 'ping works' does not mean you can access network normally and install online packages."
-            echo "You have to fix the network by yourself now."
-            echo "Leaving the script..."
-            return 1
-        fi 
-    fi
-}
-
-#check_fix_network
-
-####################################################
-#Ensure the root privelege is granted to the script
-####################################################
-function check_permission {
-    if [ $UID -ne 0 ]
-    then
-        echo
-        echo "Pemission denied!"
-        echo "To run this program successfully "
-        echo "Try to use root account or sudo command."
-        return 1
-    else
-	return 0
-    fi
-}
-
-echo "First we need to do some checking..."
-
-#Ensure root privilege;
-echo
+# To directly run this script to throttle a certain interface
+# Uncomment the following lines;
+BASE_DIR=${BASE_DIR:-${PWD%"Hadoop"*}"HadoopInitializer"}
+source $BASE_DIR"/conf_loader.sh"
+loadBasic;
+echo "Root privilege is required to run this script."
 check_permission 
-if [ $? -eq 0  ] 
+if [ $? -gt 0  ] 
 then 
-    echo "Permission Granted." 
-else 
-    echo "Permission Denied!" 
+    echo "Leaving..."
     exit 1
 fi
-
 #Ensure the network connection is okay;
-echo
 check_fix_network
-if [ $? -eq 0 ]
+if [ $? -gt 0 ]
 then
-    echo "Connection Okay!"
-else
-    echo "Failed to fix the connection."
     echo "Leaving the program..."
     exit 1
 fi
 
-read -p "Input the interface: " interface
+read -p "Input the interface you intend to throttle: " interface
+highlighter_str 2 "You can utilize `ip a` or `ifconfig` to find out the interfaces"
 read -p "The maximum rate [unit: mbps]: " max
-tc qdisc del dev $interface root
-tc qdisc add dev $interface handle 1: root htb
-tc class add dev $interface parent 1: classid 1:1 htb rate "$max"mbps
-tput setaf 1
-echo "Make some checking..."
-tput sgr0
-tc qdisc show dev $interface && tc class show dev $interface
--- tools/transfer.sh
+++ tools/transfer.sh
@@ -3,16 +3,27 @@
 # Author      : LHearen
 # E-mail      : LHearen@126.com
 # Time        : 2016-06-14 15:05
-# Description : Used to transfer files among hosts
+# Description : Used to dispatch or delegate files to a cluster
 #-------------------------------------------
 
+# Transfer files to a specified cluster of remotes by scp
+#
+# ToDo:
+# Utilize Expect to automate the password-input issue
 function transfer {
 	ips_file=$1
-	file=$2
+	src=$2
+    des=${3:-"/home/"}
 	for ip in $(cat $ips_file)
 	do
-	    scp $file $ip:/home/
+	    scp $src $ip:$des
 	done
 }
 
-transfer "etc/ips_file" "traffic_controller.sh"
+
+# To directly run this script to transfer files to a cluster
+# Uncomment the following lines;
+#BASE_DIR=${BASE_DIR:-${PWD%"Hadoop"*}"HadoopInitializer"}
+#source $BASE_DIR"/conf_loader.sh"
+#loadBasic;
+#transfer $IPS_FILE "traffic_controller.sh"
